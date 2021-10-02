1956aa3adaee2649fd73c1e0d6a3b955fd20679e
Jeremy Briones <jeremybr99@gmail.com>
2021-10-02 17:34:58 -0600

    accediento a api

---
+++ src/components/Campeones.js
@@ -0,0 +1,35 @@
+import React, { useState }  from 'react'
+import axios from 'axios';
+
+const Campeones = () => {
+
+    const [campeon, setCampeon] = useState({
+        nombre: ''
+    });
+
+    // Destructuring al state pokemon
+    const { nombre } = campeon;
+
+    const buscarCampeon = async () => {
+        const url = `https://ddragon.leagueoflegends.com/cdn/6.24.1/data/es_ES/champion.json`;
+
+        const resultado = await axios.get(url);
+        console.log(resultado);
+
+        setCampeon({
+            nombre: resultado.data.data
+        });
+    }
+
+    return ( 
+        <div>
+            <p>{nombre}</p>
+            <button onClick={() => buscarCampeon()}>Llamar</button>
+        </div>
+        
+           
+        
+     );
+}
+ 
+export default Campeones;
