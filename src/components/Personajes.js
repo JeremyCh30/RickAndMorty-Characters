50a7ff920bbe77287107b79f1bb1182e8d9765a3
Your NameReyYR02 <you@example.com>
2021-10-04 13:24:52 -0600

    Estilos nuevos, 4 personajes por fila

--- src/components/Personajes.js
+++ src/components/Personajes.js
@@ -6,17 +6,17 @@ const Personajes = ({personaje}) => {
     return ( 
         <Fragment>
         <div>
-            <img class="Logo" src={logo} alt=""/>
-        <div className="content">
-            <div className="content-characters">
+            <img class="logo" src={logo} alt=""/>
+            <div className="container">
+      <div className="row">
                 {personaje.map((item, index) => (
-                    <div key={index}>
-                        <div className="character"> 
+                    <div key={index} className="col-lg-3 col-md-6 col-sm-12 mb-4">
+                        <div  className="card" style={{ minWidth: "200px" }}> 
                           <img  src={item.image}/>
-                           <div className="info-character">
-                              <h2>{item.name}</h2>
-                              <h4>Species: {item.species}</h4>
-                              <h4>Status: {item.status}</h4>
+                           <div className="card-body">
+                              <h2 className="card-title">{item.name}</h2>
+                              <h4 className="card-text">Species: {item.species}</h4>
+                              <h4 className="card-text">Status: {item.status}</h4>
                             </div>
                          </div>
                          </div>
