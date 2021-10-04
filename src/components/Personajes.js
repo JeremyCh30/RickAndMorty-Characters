47036b373a727d8e1afe897417b55323d1f423d4
Your NameReyYR02 <you@example.com>
2021-10-03 19:55:35 -0600

    Estilo de la página

--- src/components/Personajes.js
+++ src/components/Personajes.js
@@ -1,25 +1,30 @@
-import React, { useState }  from 'react'
-import axios from 'axios';
+import React, {Fragment} from 'react';
+import logo from '../images/logo.png'
+import './Personajes.css';
 
 const Personajes = ({personaje}) => {
     return ( 
+        <Fragment>
         <div>
-            <div>
+            <img class="Logo" src={logo} alt=""/>
+        <div className="content">
+            <div className="content-characters">
                 {personaje.map((item, index) => (
-                    <div> key={index}
-                        <div>
-                          <img src={item.image}/>
-                           <div>
-                              <h5>{item.name}</h5>
-                              <hr />
-                              <p>Species: {item.species}</p>
-                              <p>Location: {item.location.name}</p>
+                    <div key={index}>
+                        <div className="character"> 
+                          <img  src={item.image}/>
+                           <div className="info-character">
+                              <h2>{item.name}</h2>
+                              <h4>Species: {item.species}</h4>
+                              <h4>Status: {item.status}</h4>
                             </div>
                          </div>
-                    </div>
+                         </div>
                 ))}
                 </div>
             </div>
+            </div>
+            </Fragment>
      );
 }
  
